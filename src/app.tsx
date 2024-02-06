24187da2d5427c9661b0b0b4caa1d31171e3dcfb
LuizinhoGustavo <lggustavo329@gmail.com>
2024-02-05 22:54:16 -0300

    Criação e modificação de arquivos

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+
+export function App() { //Retona de maneira mais fácil
+  return <h1>Hello World!</h1>//Componentes JSX - HTML convertido para JS
+}
+
+ 
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './app' // Retorna a função independente do nome que ela estiver
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render( //Buscar a DIV "Root" do HTML (como se fosse getElementById)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
