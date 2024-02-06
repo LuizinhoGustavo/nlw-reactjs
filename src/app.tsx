d4e39904d9c31bb554f31d32926ad07d9b0d24c5
LuizinhoGustavo <lggustavo329@gmail.com>
2024-02-06 19:54:09 -0300

    Finalização de 85% do layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,45 @@
+import logo from './assets/logoNLW.svg' // No React podemos importar o vetor (svg) dessa forma e apenas coloca-lo no projeto de maneira mais fácil com a tab 'img'
+import { NoteCard } from './components/note-card'
 
 export function App() { //Retona de maneira mais fácil
-  return <h1>Hello World!</h1>//Componentes JSX - HTML convertido para JS
+  return( 
+    // No react nao podemos colocar 2 elementos em baixo do outro se eles não tiverem envoltos por uma tag meior, como uma div.
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt='NLW Expert'/>
+
+      <form className='w-full'>
+        <input 
+          type="text" 
+          placeholder='Busque Suas Notas'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          />
+      </form>
+
+      <div className='h-px bg-slate-700' /> 
+
+      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+
+        <div className='rouded-md bg-slate-700 p-5 space-y-3'>
+          <span className='text-sm font-medium text-slate-200'>Adicionar nota</span>
+          <p className='text-sm leading-6 text-slate-400'>
+            Grave uma nota em áudio que será convertida para texto automaticamente.
+          </p>
+        </div>
+
+        <NoteCard/> {/*Importação do Component*/}
+
+      </div>
+
+    </div>
+    )//Componentes JSX - HTML convertido para JS
 }
 
+/*
+Anotações Imporatantes -
+
+  * No React não usamos a palavra 'Class' e sim 'ClassName'
+  * As medidas do Tailwind são múltiplas de 4, ou seja o mx-1 é igual a 4px,...
+  * space-y -> Essa funionalidade do Tailwind aplica um margin-top em todos os elmentos que não seja o primeiro
+  * Comentários dentro de um bloco jsx é entre {} e com o símbolo de comentário dentro
+*/
  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
+  content: [ //Assim como diz na Doc, é necessário mudar o content para esse código.
+  //O content determina quais arquivos do projeto podem conter classes CSS para estilização
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { //"Extend" é usado para extender as propriedades do TailWind que já vem por padrão, exemplo Fonte, Cor
+    extend: {
+      fontFamily: { //Mudas os estilos de fontes
+        sans: ['Inter', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
